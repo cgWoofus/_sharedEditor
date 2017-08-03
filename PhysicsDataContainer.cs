d0585e4f2eda764b828e80168aab4679a72e18c9
cgWoofus <charlesbryanguerrero@yahoo.com>
2017-08-03 17:13:25 +0800

    passing Initial

---
+++ PhysicsDataContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+public class PhysicsDataContainer
+{
+    [XmlArray("Physics_Database")]
+    [XmlArrayItem("Physics_Data")]
+    public List<PhysicsDatabase_Data> data = new List<PhysicsDatabase_Data>();
+}
+public class PhysicsDatabase_Data
+{
+    [XmlAttribute("Name")]
+    public string name;
+
+    [XmlElement("Material_ID")]
+    public int _matId;
+
+    [XmlElement("Mass")]
+    public float _mass;
+
+    [XmlElement("Linear_Drag")]
+    public float _lDrag;
+
+    [XmlElement("Angular_Drag")]
+    public float _ADrag;
+
+    [XmlElement("Gravity_Scale")]
+    public float _gScale;
+
+}
+
+public class PhysicsObjectsCollection
+{
+    
+    [XmlAttribute("Name")]
+    public string _objectname;
+
+
+  //  [XmlAttribute("Constraints")]
+    //public string _objectname;
+
+
+}
+
---
+++ PhysicsToken.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class PhysicsToken 
+{
+    PhysicsDatabase_Data _sample = new PhysicsDatabase_Data();
+    PhysicsDataContainer _mycontainer;
+    string name;
+    // Use this for initialization
+    public PhysicsToken(PhysicsDataContainer _container)
+    {
+        _mycontainer = _container;
+        Enable();
+    }
+
+    public void StoreTextureData()
+    {
+        // sample data set
+        _sample.name = "Wood";
+        _sample._mass = 10f;
+        _sample._lDrag = 0f;
+        _sample._ADrag = 0.05f;
+        _sample._gScale = 1f;
+        _sample._matId = 0;
+    }
+    void addData()
+    {
+        StaticSaveData_Editor.AddActorData<PhysicsDatabase_Data>(_mycontainer.data, _sample);
+    }
+
+    void Enable()
+    {
+        StaticSaveData_Editor.BeforeSaveProcess += StoreTextureData;
+        StaticSaveData_Editor.BeforeSaveProcess += addData;
+    }
+    public void Disable()
+    {
+        StaticSaveData_Editor.BeforeSaveProcess -= StoreTextureData;
+        StaticSaveData_Editor.BeforeSaveProcess -= addData;
+    }
+    public void LoadData()
+    {
+        // status = data.status;
+    }
+
+}
